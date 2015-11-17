8b24051302269eb315ded21f083d395a2c64de2a
Achal Dave <achalddave@live.com>
2015-11-17 11:35:21 -0500

    Add script to list all HIT ids

---
+++ list_hit_ids.py
@@ -0,0 +1,13 @@
+import argparse
+
+import simpleamt
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(parents=[simpleamt.get_parent_parser()],
+              description="List active HITs")
+  args = parser.parse_args()
+
+  mtc = simpleamt.get_mturk_connection_from_args(args)
+
+  for hit in mtc.get_all_hits():
+    print hit
