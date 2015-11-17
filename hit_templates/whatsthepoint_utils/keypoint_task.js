094b86d940f0773be87d9742c251184f5fd3b516
Achal Dave <achalddave@live.com>
2015-11-17 14:20:23 -0500

    Update input format, add documentation, change formatting in keypoint task

--- hit_templates/whatsthepoint_utils/keypoint_task.js
+++ hit_templates/whatsthepoint_utils/keypoint_task.js
@@ -1,7 +1,18 @@
 var VG = (function(vg) {
-  //task_input must have:
-  //  image_url, obj_name, obj_plural, gt.bbox, gt.min_num, gt.max_num
-  vg.PointTask = function(container_div, task_input) {
+  /***
+   * Setup a keypoint annotation task using VG.PointDrawer.
+   *
+   * @param {jQuery div} container_div HTML div wrapped by jQuery.
+   * @param {object} task_input Input for the task. Contains the following keys
+   *     <ul>
+   *     <li> image_id
+   *     <li> image_url
+   *     <li> annotation (Optionally contains groundtruth)
+   *     <li> object_name
+   *     <li> object_name_plural
+   *     </ul>
+   */
+  vg.KeypointTask = function(container_div, task_input) {
     var that = (this === vg ? {} : this);
     var enabled = false;
 
@@ -16,42 +27,42 @@ var VG = (function(vg) {
     max_height -= $('#c-buttons-div').height();
     var images_div = $('#c-imgs-div');
     var bbox_drawer_options = {
-      max_height: max_height,
-      max_width: images_div.width(),
+      max_height : max_height,
+      max_width : images_div.width(),
 
-      gt: task_input.gt,
-      obj_singular: task_input.obj_name,
-      obj_plural: task_input.obj_plural
+      gt : task_input.annotation.groundtruth,
+      obj_singular : task_input.object_name,
+      obj_plural : task_input.object_plural
     };
-    var bbox_drawer = new VG.PointDrawer(
-      drawer_div, image_url, null, bbox_drawer_options);
+    var bbox_drawer =
+        new VG.PointDrawer(drawer_div, image_url, null, bbox_drawer_options);
 
     var timer = new VG.Timer();
 
     that.GetAnswerIfValid = function() {
       return {
-        'answer': bbox_drawer.GetAnswer(),
-        'time': timer.total(),
-        'eval': bbox_drawer.EvalAnswer()
+        'answer' : bbox_drawer.GetAnswer(),
+        'time' : timer.total(),
+        'eval' : bbox_drawer.EvalAnswer()
       };
-    }
+    };
 
     that.enable = function() {
       enabled = true;
       bbox_drawer.enable();
       timer.start();
-    }
+    };
 
     that.disable = function() {
       enabled = false;
       bbox_drawer.disable();
       timer.stop();
-    }
+    };
 
     that.disable();
 
     return that;
-  }
+  };
 
   return vg;
 
