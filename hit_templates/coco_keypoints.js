c9d5303d4681b97104938db95a41c8543a143f9f
Achal Dave <achalddave@live.com>
2015-11-19 14:15:46 -0500

    Use sample data with multiple images

--- hit_templates/coco_keypoints.js
+++ hit_templates/coco_keypoints.js
@@ -1,50 +1,53 @@
 // Define some default input.
 var DEFAULT_INPUT = [
   {
-    "image_id" : 167765,
-    "image_url" : "http://mscoco.org/images/167765",
-    "annotation" : {
-      "image_id" : 167765,
-      "bbox" : [ 0.0, 1.44, 182.65, 629.93 ],
-      "id" : 183813,
-      "groundtruth" : {
-        "bbox" : {"x" : 0.0, "y" : 1.44, "w" : 182.65, "h" : 629.93}
-      },
-    },
-    "object_name": "elbow",
-    "object_name_plural": "elbows",
-    "annotation_id" : 183813
+    "object_name_plural": "necks",
+    "image_id": 196842,
+    "object_name": "neck",
+    "image_url": "http://mscoco.org/images/196842",
+    "annotation_id": 183022,
+    "annotation": {
+      "area": 3817.67415,
+      "iscrowd": 0,
+      "image_id": 196842,
+      "bbox": [273.14, 82.25, 72.6, 129.68],
+      "groundtruth": {"y": 82.25, "x": 273.14, "w": 72.6, "h": 129.68},
+      "category_id": 1,
+      "id": 183022
+    }
   },
   {
-    "image_id" : 167765,
-    "image_url" : "http://mscoco.org/images/167765",
-    "annotation" : {
-      "image_id" : 167765,
-      "bbox" : [ 0.0, 1.44, 182.65, 629.93 ],
-      "id" : 183813,
-      "groundtruth" : {
-        "bbox" : {"x" : 0.0, "y" : 1.44, "w" : 182.65, "h" : 629.93}
-      },
-    },
-    "object_name": "elbow",
-    "object_name_plural": "elbows",
-    "annotation_id" : 183813
+    "object_name_plural": "necks",
+    "image_id": 44474,
+    "object_name": "neck",
+    "image_url": "http://mscoco.org/images/44474",
+    "annotation_id": 183024,
+    "annotation": {
+      "area": 52541.14914999998,
+      "iscrowd": 0,
+      "image_id": 44474,
+      "bbox": [33.42, 11.94, 303.68, 407.76],
+      "groundtruth": {"y": 11.94, "x": 33.42, "w": 303.68, "h": 407.76},
+      "category_id": 1,
+      "id": 183024
+    }
   },
   {
-    "image_id" : 167765,
-    "image_url" : "http://mscoco.org/images/167765",
-    "annotation" : {
-      "image_id" : 167765,
-      "bbox" : [ 0.0, 1.44, 182.65, 629.93 ],
-      "id" : 183813,
-      "groundtruth" : {
-        "bbox" : {"x" : 0.0, "y" : 1.44, "w" : 182.65, "h" : 629.93}
-      },
-    },
-    "object_name": "elbow",
-    "object_name_plural": "elbows",
-    "annotation_id" : 183813
-  },
+    "object_name_plural": "necks",
+    "image_id": 382669,
+    "object_name": "neck",
+    "image_url": "http://mscoco.org/images/382669",
+    "annotation_id": 183026,
+    "annotation": {
+      "area": 10161.016499999998,
+      "iscrowd": 0,
+      "image_id": 382669,
+      "bbox": [237.59, 107.09, 110.99, 241.92],
+      "groundtruth": {"y": 107.09, "x": 237.59, "w": 110.99, "h": 241.92},
+      "category_id": 1,
+      "id": 183026
+    }
+  }
 ];
 
 var input = null;
